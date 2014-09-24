8dec16a3c15c30b129cd9723d28e5c27806fd7c6
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-09-24 11:56:38 +0330

    Added a thin caching layer on top of dbaccess/food.go

--- dbaccess/food.go
+++ dbaccess/food.go
@@ -1,17 +1,10 @@
 package dbaccess
 
-import "log"
-
-// Food stores basic data about foods
-type Food struct {
-	Name           string
-	Name_fa        string
-	Description    string
-	Description_fa string
-	Price          int64
-	Thumbnail      string
-	Pictures       []string
-}
+import (
+	"log"
+	"sync"
+	"time"
+)
 
 const foodsOfACatSql = `
   SELECT f.id, f.name, f.name_fa, f.description, f.description_fa, f.price,
@@ -21,53 +14,101 @@ const foodsOfACatSql = `
     AND f.food_category_id = $1
 `
 
-type foodResult struct {
-	Food
-	error
-}
+type (
+	// Food stores basic data about foods
+	Food struct {
+		Name           string
+		Name_fa        string
+		Description    string
+		Description_fa string
+		Price          int64
+		Thumbnail      string
+		Pictures       []string
+	}
 
-type foodRequest struct {
-	id         int64
-	resultChan chan<- foodResult
-	errChan    chan<- error
-}
+	foodResult struct {
+		Food
+		error
+	}
 
-var foodRequestsChan = make(chan foodRequest)
+	foodRequest struct {
+		id         int64
+		resultChan chan<- foodResult
+		errChan    chan<- error
+	}
+
+	foodCacheStore struct {
+		time  time.Time
+		foods []Food
+	}
+)
+
+var (
+	foodRequestsChan = make(chan foodRequest)
+	foodCache        = struct {
+		sync.RWMutex
+		c map[int64]foodCacheStore
+	}{c: make(map[int64]foodCacheStore)}
+)
 
 func init() {
 	for i := 0; i < 20; i++ {
 		go func() {
 			for req := range foodRequestsChan {
+				foodCache.RLock()
+				cache, ok := foodCache.c[req.id]
+				foodCache.RUnlock()
 
-				type dbfood struct {
-					ID             int64
-					Name           string
-					Name_fa        string
-					Description    string
-					Description_fa string
-					Price          int64
-					Thumbnail      string
-				}
+				if ok && cacheIsRecent(cache.time) {
+					req.errChan <- nil
+					for _, food := range cache.foods {
+						req.resultChan <- foodResult{food, nil}
+					}
+				} else {
+					allFoods := make([]Food, 0, 10)
 
-				var foods []dbfood
-
-				req.errChan <- db.Select(&foods, foodsOfACatSql, req.id)
-
-				for _, food := range foods {
-					picsChan, picErrc := getPicsOfFood(food.ID)
-
-					err := <-picErrc
-					pics := []string{}
-
-					for pic := range picsChan {
-						if pic.error == nil {
-							pics = append(pics, pic.string)
-						} else {
-							log.Print(pic.error)
-						}
+					type dbfood struct {
+						ID             int64
+						Name           string
+						Name_fa        string
+						Description    string
+						Description_fa string
+						Price          int64
+						Thumbnail      string
 					}
 
-					req.resultChan <- foodResult{Food{food.Name, food.Name_fa, food.Description, food.Description_fa, food.Price, food.Thumbnail, pics}, err}
+					var foods []dbfood
+
+					req.errChan <- db.Select(&foods, foodsOfACatSql, req.id)
+
+					for _, food := range foods {
+						picsChan, picErrc := getPicsOfFood(food.ID)
+
+						err := <-picErrc
+						pics := []string{}
+
+						for pic := range picsChan {
+							if pic.error == nil {
+								pics = append(pics, pic.string)
+							} else {
+								log.Print(pic.error)
+							}
+						}
+
+						foodie := Food{
+							food.Name, food.Name_fa,
+							food.Description, food.Description_fa,
+							food.Price, food.Thumbnail, pics,
+						}
+
+						allFoods = append(allFoods, foodie)
+
+						req.resultChan <- foodResult{foodie, err}
+					}
+
+					foodCache.Lock()
+					foodCache.c[req.id] = foodCacheStore{time.Now(), allFoods}
+					foodCache.Unlock()
 				}
 
 				close(req.resultChan)
