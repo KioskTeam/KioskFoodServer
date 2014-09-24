0f69b91262b38414e0dd65aa4448b389f247589b
Mohammad Sadegh Khoeini(MSK) <mskco.tp@gmail.com>
2014-09-24 09:25:06 +0330

    Refactored dbaccess/food.go to be bounded

--- dbaccess/food.go
+++ dbaccess/food.go
@@ -26,45 +26,61 @@ type foodResult struct {
 	error
 }
 
+type foodRequest struct {
+	id         int64
+	resultChan chan<- foodResult
+	errChan    chan<- error
+}
+
+var foodRequestsChan = make(chan foodRequest)
+
+func init() {
+	for i := 0; i < 20; i++ {
+		go func() {
+			for req := range foodRequestsChan {
+
+				type dbfood struct {
+					ID             int64
+					Name           string
+					Name_fa        string
+					Description    string
+					Description_fa string
+					Price          int64
+					Thumbnail      string
+				}
+
+				var foods []dbfood
+
+				req.errChan <- db.Select(&foods, foodsOfACatSql, req.id)
+
+				for _, food := range foods {
+					picsChan, picErrc := getPicsOfFood(food.ID)
+
+					err := <-picErrc
+					pics := []string{}
+
+					for pic := range picsChan {
+						if pic.error == nil {
+							pics = append(pics, pic.string)
+						} else {
+							log.Print(pic.error)
+						}
+					}
+
+					req.resultChan <- foodResult{Food{food.Name, food.Name_fa, food.Description, food.Description_fa, food.Price, food.Thumbnail, pics}, err}
+				}
+
+				close(req.resultChan)
+			}
+		}()
+	}
+}
+
 func getFoodsOfCat(id int64) (<-chan foodResult, <-chan error) {
 	c := make(chan foodResult)
 	errc := make(chan error, 1)
 
-	go func() {
-
-		type dbfood struct {
-			ID             int64
-			Name           string
-			Name_fa        string
-			Description    string
-			Description_fa string
-			Price          int64
-			Thumbnail      string
-		}
-
-		var foods []dbfood
-
-		errc <- db.Select(&foods, foodsOfACatSql, id)
-
-		for _, food := range foods {
-			picsChan, picErrc := getPicsOfFood(food.ID)
-
-			err := <-picErrc
-			pics := []string{}
-
-			for pic := range picsChan {
-				if pic.error == nil {
-					pics = append(pics, pic.string)
-				} else {
-					log.Print(pic.error)
-				}
-			}
-
-			c <- foodResult{Food{food.Name, food.Name_fa, food.Description, food.Description_fa, food.Price, food.Thumbnail, pics}, err}
-		}
-
-		close(c)
-	}()
+	foodRequestsChan <- foodRequest{id, c, errc}
 
 	return c, errc
 }
